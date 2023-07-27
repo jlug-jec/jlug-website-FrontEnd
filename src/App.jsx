8f2ab91a28fea231511db782ccd93140988825ba
Aryan Agrawal <aryanagr219@gmail.com>
2023-07-27 21:26:46 +0530

    created all the components and done till 30:50

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./style";
+import { Navbar, Hero, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials,
+} from "./components";
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
-      <div>
-        
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+          hello 
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <CTA/>
+          <Clients/>
+          <Footer/>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      nav
+    </div>
+  )
+}
+
+export default Navbar
