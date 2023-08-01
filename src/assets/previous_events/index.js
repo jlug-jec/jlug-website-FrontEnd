f590d916a3eb77715c6f99065d98866a012d79cd
Anushk Jain <anushk2201@gmail.com>
2023-08-01 12:46:49 +0530

    Event Info added

--- src/assets/previous_events/index.js
+++ src/assets/previous_events/index.js
@@ -1,9 +1,31 @@
 import AlmaMatters from './AlmaMatters.jpeg';
 import EngineeringDay from './EngineeringDay.jpg';
 import LinkedInSession from './LinkedInSession.jpg';
+import AkashSession from './AkashSession.jpeg';
+import AlumniMeets from './AlumniMeets.jpeg';
+import AWS from './AWS.jpeg';
+import HarshLalwani from './HarshLalwani.jpeg';
+import HenryHarwin from './HenryHarwin.jpeg';
+import JecX from './JecX.jpeg';
+import Mashaal from './Mashaal.jpeg';
+import PanelDiscussion from './PanelDiscussion.jpeg';
+import RohitNema from './RohitNema.jpeg';
+import SushilKumar from './SushilKumar.jpeg';
+import WingCommander from './WingCommander.jpeg';
 
 export{
     AlmaMatters,
     EngineeringDay,
-    LinkedInSession
+    LinkedInSession,
+    AkashSession,
+    AlumniMeets,
+    AWS,
+    HarshLalwani,
+    HenryHarwin,
+    JecX,
+    Mashaal,
+    PanelDiscussion,
+    RohitNema,
+    SushilKumar,
+    WingCommander
 }
