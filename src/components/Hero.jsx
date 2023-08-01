96b73cf4b3b377c06df221eaa1b224b97a99e41a
Aryan Agrawal <aryanagr219@gmail.com>
2023-08-01 22:46:49 +0530

    hero khali

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import {discount, robot} from "../assets";
-import GetStarted from "./GetStarted"
-const Hero = () => {
-  return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+// import React from 'react'
+// import {discount, robot} from "../assets";
+// import GetStarted from "./GetStarted"
+// const Hero = () => {
+//   return (
+//     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+//       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
 
-      </div>
-    </section>
-  )
-}
+//       </div>
+//     </section>
+//   )
+// }
 
-export default Hero
+// export default Hero
