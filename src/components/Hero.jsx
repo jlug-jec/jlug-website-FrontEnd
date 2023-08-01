1223f0a0cf593ae10df127dee3a515ac30636440
Aryan Agrawal <aryanagr219@gmail.com>
2023-08-01 22:44:49 +0530

    adha adhoora

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {discount, robot} from "../assets";
+import GetStarted from "./GetStarted"
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+      </div>
+    </section>
   )
 }
 
