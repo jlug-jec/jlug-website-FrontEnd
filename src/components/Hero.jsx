00caf8b7b74e8218bf553cf1662141008da8923d
Anushk Jain <anushk2201@gmail.com>
2023-08-02 23:11:14 +0530

    Getting Started Amends

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,12 @@ const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-        <div className="flex flex-row items-center py-[px] px-4 bg-discount gradient rounded-[10px] mb-2">
-          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p className={`${styles.paragraph} ml-2`} >
-            <span className="text-white">20%</span>
-            Discount for{" "}
-            <span className="text-white">1 Month</span>
-            Account
+        <div className="flex flex-row items-center py-[px] px-0 bg-discount gradient rounded-[10px] mb-2">
+          <p className={`${styles.paragraph} `} >
+            The{" "}
+            <span className="text-white">Technical{" "}</span>
+            Society of{" "}
+            <span className="text-white">JEC</span>
           </p>
         </div>
 
