e7c888eae29c8d5d3dafb7f9e366ac60509d4c1e
Anushk Jain <anushk2201@gmail.com>
2023-08-02 23:05:38 +0530

    Favicon Domne

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             {/* Presenting You <br className="sm:block hidden"/>{" "} */}
-            <span className="text-gradient">The JLUG</span>{" "}  </h1>
+            <span className="text-gradient">JLUG</span>{" "}  </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <a href="https:google.com"><GetStarted /></a>
           </div>
