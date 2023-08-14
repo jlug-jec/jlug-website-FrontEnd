d52f290e5ebb16c3cd01a14c20a5919e36f84fc5
Anushk Jain <anushk2201@gmail.com>
2023-08-14 05:58:41 +0530

    Changes For The Deployment Part 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Button = ({ styles }) => (
-  <Link to="/jlug-website-FrontEnd/pagenotfound/*"><button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+  <Link to="/pagenotfound"><button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
     Join Us
   </button></Link>
 );
