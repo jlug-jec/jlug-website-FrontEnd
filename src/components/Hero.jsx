80070f3d234d3f0c698de250875a899517e02c91
Anushk Jain <anushk2201@gmail.com>
2023-08-14 06:17:17 +0530

    Removed Hero Image In Mobile View

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,14 +34,14 @@ const Hero = () => {
         </p>
       </div>
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={home2} alt="billing" className='w-[100%] h-[100%px] relative z-[5]' />
+        <img src={home2} alt="billing" className='w-[100%] h-[100%px] relative z-[5] hidden sm:block' />
         {/* gradient start */}
         <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
       </div>
-      <div className={`ss:hidden ${styles.flexCenter}`}>
+      <div className={`ss:hidden ${styles.flexCenter} mb-7`}>
       <Link to="/pagenotfound/"><GetStarted /></Link>
       </div>
     </section>
