f8424a9f7ee1b4b55b160889cd397a7dee301f88
Harshit M <harshitmeh111@gmail.com>
2023-08-15 15:21:46 +0530

    Reduced images size

--- src/assets/extra/index.js
+++ src/assets/extra/index.js
@@ -1,13 +1,7 @@
-import home from './possibility.png';
-import home2 from "./possibility2.png";
-import ai from './ai.png';
-import boy2 from './Boy2.png';
+import home from "./possibility.png";
 import bg from './bg.png';
 
 export{
     home,
-    ai,
-    boy2,
-    home2,
     bg
 }
